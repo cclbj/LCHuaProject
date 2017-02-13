5c99ed19f007f30201728a29bd3d5ce8c66526b2
lccdl <lcc_dl@163.com>
2017-02-13 16:23:00 +0800

    更新： 整合代码

---
+++ LCHuaProject/Main/Topic(专题)/LCTopicIndexCell.swift
@@ -0,0 +1,24 @@
+//
+//  LCTopicIndexCell.swift
+//  LCHuaProject
+//
+//  Created by lcc on 2017/2/13.
+//  Copyright © 2017年 early bird international. All rights reserved.
+//
+
+import UIKit
+
+class LCTopicIndexCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 platform :ios, '8.0'
 use_frameworks!
 
+pod 'Cartography', '~> 1.0.1'
 
 target 'LCHuaProject' do
     pod 'SwiftyJSON', '~> 3.1.4'
